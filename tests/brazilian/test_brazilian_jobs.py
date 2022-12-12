d8a1e440ab98a022141a4824680858738f2c710a
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-12-12 15:24:46 -0300

    Feat: add reqs 10, 11 e 12

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    read_file = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for translated_job in read_file:
+        assert translated_job.get("title") is not None
+        assert translated_job.get("salary") is not None
+        assert translated_job.get("type") is not None
+        assert translated_job.get("titulo") is None
+        assert translated_job.get("salario") is None
+        assert translated_job.get("tipo") is None
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,19 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+job_1 = {"min_salary": 100, "max_salary": 1000, "date_posted": '2001-02-01'}
+job_2 = {"min_salary": 50, "max_salary": 2000, "date_posted": ''}
+job_3 = {"min_salary": '', "max_salary": 3000, "date_posted":  '2021-02-01'}
+job_4 = {"min_salary": 200, "max_salary": '', "date_posted": '2011-02-01'}
+
+MOCK_MAX = [job_1, job_2, job_3, job_4]
+MOCK_MIN = [job_1, job_2, job_3, job_4]
+MOCK_DATE = [job_1, job_2, job_3, job_4]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(MOCK_MAX, "max_salary")
+    assert MOCK_MAX == [job_3, job_2, job_1, job_4]
+    sort_by(MOCK_MIN, "min_salary")
+    assert MOCK_MIN == [job_2, job_1, job_4, job_3]
+    sort_by(MOCK_DATE, "date_posted")
+    assert MOCK_DATE == [job_3, job_4, job_1, job_2]
